b061616818c0b8308d9af394879a4802f9706eb1
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-01-27 17:07:05 -0300

    added weather node

---
+++ pages/api/weather.js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+
+async function get_weather(req, response) {
+    const api_secret = process.env.WEATHER_API_KEY
+
+    const weather_response = await fetch(`api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&appid=${api_secret}`);
+    const wheather = await weather_response.json();
+
+    response.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate')
+    response.setHeader('Access-Control-Allow-Origin', '*')
+
+    response.statusCode = 200;
+    response.json({
+        wheather
+    });
+}
+
+export default get_news;
